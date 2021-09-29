875678372bafc465a367567fd28ce8d6c7bfea3f
Junya Qiao <junyaqiao@gmail.com>
2021-09-29 18:11:49 -0400

    fetch api

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var searchBtn = document.querySelector(".searchBtn");
 searchBtn.addEventListener("click", function(){
     city = document.querySelector(".input").value;
     console.log(city);
-    fetch('api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c7a06032b7a947e2ad991c8e63f709c1')
+    fetch(' http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c7a06032b7a947e2ad991c8e63f709c1')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => "name error");
