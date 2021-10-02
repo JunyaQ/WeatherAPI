30773f6412b1d1f90194d727f863155359650ba8
Junya Qiao <junyaqiao@gmail.com>
2021-10-02 12:01:20 -0400

    get information need to make the page

--- js/script.js
+++ js/script.js
@@ -11,20 +11,31 @@ searchBtn.addEventListener("click", function(){
         console.log(data);
         var city = data['name'];
         console.log("city: "+city);
-        var date = moment().format('L');   
+        var lat = data['coord']['lat'];
+        var lon = data['coord']['lon'];
+        fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=daily&appid=c7a06032b7a947e2ad991c8e63f709c1')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+                 
+        var date ;
         console.log("date: "+date);
-        var temp = data['main']['temp'];
+        var temp ;
         console.log("temp: "+temp);
-        var wind;
-        var humidity;
+        var wind ;
+        console.log("wind: "+wind);
+        var humidity ;
+        console.log("humidity: "+humidity);
         var uv;
+        })
     })
    
     .catch(error => "name error")
    
 })
 
-
+//https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 //c7a06032b7a947e2ad991c8e63f709c1
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
